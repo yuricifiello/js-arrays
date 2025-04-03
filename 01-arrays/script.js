9d36cd6bab3e56ef3f7bff6a0f8539408497222d
yuricifiello <yuricifiello@gmail.com>
2025-04-03 15:21:12 +0200

    first commit

---
+++ 01-arrays/script.js
@@ -0,0 +1,33 @@
+const teachers = [
+  'Nathan',
+  'Ed',
+  'Fabio',
+  'Phil',
+  'Carlo',
+  'Lewis',
+  'Luca'
+]; // NON MODIFICARE QUESTA VARIABILE
+
+// 1. Utilizzando un indice sull'array teachers, cambia il valore della
+// variabile fourthTeacher qui sotto in modo che sia il quarto insegnante elencato nell'array
+const fourthTeacher = null;
+
+// 2. Sostituisci il quinto insegnante nell'array teachers con 'Patrick'
+
+// 3. Rimuovi l'ultimo insegnante dall'array e salvalo nella variabile lastTeacher qui sotto
+const lastTeacher = null;
+
+// 4. Rimuovi il primo insegnante dall'array e salvalo nella variabile firstTeacher qui sotto
+const firstTeacher = null;
+
+// 5. Aggiungi un insegnante di nome 'Vanessa' alla fine dell'array teachers
+
+// 6. Aggiungi un insegnante di nome 'Sarah' all'inizio dell'array teachers
+
+// 7. Trova l'indice dell'insegnante 'Lewis' nell'array teachers
+// e salvalo nella variabile lewisIndex
+const lewisIndex = null;
+
+// 8. Verifica se l'array teachers è vuoto
+// e salva il risultato nella variabile isTeachersEmpty
+const isTeachersEmpty = null;
---
+++ 02-arrays-bonus/script.js
@@ -0,0 +1,26 @@
+const teachers = [
+  'Nathan',
+  'Ed',
+  'Fabio',
+  'Phil',
+  'Carlo',
+  'Lewis',
+  'Luca'
+]; // NON MODIFICARE QUESTA VARIABILE
+
+// 1. Inverti l'ordine degli insegnanti nell'array teachers
+// e salva il risultato nella variabile reversedTeachers
+const reversedTeachers = null;
+
+// 2. Crea un nuovo array chiamato 'longNames' che contenga solo gli insegnanti
+// con un nome di lunghezza maggiore o uguale a 5 caratteri
+const longNames = null;
+
+// 3. Rimuovi 'Ed' dall'array teachers
+
+// 4. Verifica se 'Fabio' è presente nell'array teachers
+// e salva il risultato nella variabile isFabioPresent
+const isFabioPresent = null;
+
+// 5. Unisci tutti gli insegnanti nell'array teachers in una stringa  separata da virgole e salvala nella variabile teachersString
+const teachersString = null;
